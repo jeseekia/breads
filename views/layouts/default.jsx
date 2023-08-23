70ee7dba06c577882db49b843f2e446fcaecd779
Jeseekia Vaughn <jeseekia@gmail.com>
2023-08-22 20:27:06 -0400

    Refactor the default layout and add navigation

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -3,19 +3,23 @@ const React = require('react')
 function Default(html) {
     return (
         <html>
-           <head>
+            <head>
                 <title>
                     Default
                 </title>
-           </head> 
-           <body>
-            <h1>
-                HTML Rendered!
-            </h1>
-            <div className="container">
-                {html.children}
-            </div>
-           </body>
+            </head> 
+            <body>
+                <div className="wrapper">
+                    <header>
+                        <h1>
+                            <a href="/breads">BreadCRUD</a>
+                        </h1>
+                    </header>
+                    <div className="container">
+                        {html.children}
+                    </div>
+                </div>
+            </body>
         </html>
     )
 }
