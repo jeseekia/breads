1e6828dcf91bad6aeced5aa09762eed174c32a8e
Jeseekia Vaughn <jeseekia@gmail.com>
2023-08-24 20:48:51 -0400

    Create a favicon for and include attribution

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -8,6 +8,7 @@ function Default(html) {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" integrity="sha512-EZLkOqwILORob+p0BXZc+Vm3RgJBOe1Iq/0fiI7r/wJgzOFZMlsqTa29UEl6v6U6gsV4uIpsNZoV32YZqrCRCQ==" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="/main.css" />
+                <link rel="shortcut icon" type="image/png" href="/assets/bread.png"/>
             </head> 
             <body>
                 <div className="wrapper">
@@ -20,6 +21,7 @@ function Default(html) {
                         {html.children}
                     </div>
                 </div>
+                <a href="https://www.flaticon.com/free-icons/bread" title="bread icons">Bread icons created by Smashicons - Flaticon</a>
             </body>
         </html>
     )
